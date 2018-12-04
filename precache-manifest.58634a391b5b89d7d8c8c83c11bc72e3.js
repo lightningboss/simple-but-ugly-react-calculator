c68a0378ce610dec67bf55222d00eb0b9c3a2b36
Marc Nitzsche <marc.nitzsche@live.de>
2018-12-04 15:21:29 +0100

    Updates

---
+++ precache-manifest.58634a391b5b89d7d8c8c83c11bc72e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "783754e56a63eea61ba2",
+    "url": "/simple-but-ugly-react-calculator/static/js/runtime~main.783754e5.js"
+  },
+  {
+    "revision": "b5622990cd1146080bec",
+    "url": "/simple-but-ugly-react-calculator/static/js/main.b5622990.chunk.js"
+  },
+  {
+    "revision": "25779c33aabab879cbcd",
+    "url": "/simple-but-ugly-react-calculator/static/js/1.25779c33.chunk.js"
+  },
+  {
+    "revision": "b5622990cd1146080bec",
+    "url": "/simple-but-ugly-react-calculator/static/css/main.11c33069.chunk.css"
+  },
+  {
+    "revision": "bdb9209c67a75ef72fe6c127a945dbf8",
+    "url": "/simple-but-ugly-react-calculator/index.html"
+  }
+];
